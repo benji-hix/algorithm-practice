8f8b27d51ab12f4cc1944d6e6d420242f3f9a8e3
Benji Hix <benji.hix@outlook.com>
2023-08-29 08:55:23 -0700

    second js algo

--- ap-2023-08-29.js
+++ ap-2023-08-29.js
@@ -26,19 +26,62 @@ const separator5 = ", ";
 const expected5 = "";
 
 
-function join(arr, separator) {
+function join(arr, separator = ', ') {
     var newString = "";
 
     for (i = 0; i < arr.length; i++) {
         newString += arr[i];
-        if ( i != arr.length -1) newString += separator;
+        ( i != arr.length -1) ? newString += separator : false;
     }
 
     return newString;
 }
 
-console.log(join(arr1, separator1) + " should be " + expected1);
+console.log(join(arr1) + " should be " + expected1);
 console.log(join(arr2, separator2) + " should be " + expected2);
 console.log(join(arr3, separator3) + " should be " + expected3);
-console.log(join(arr4, separator4) + " should be " + expected4);
+console.log(join(arr4) + " should be " + expected4);
 console.log(join(arr5, separator5) + " should be " + expected5);
+
+
+
+// algo 2
+/* 
+  Acronyms
+
+  Create a function that, given a string, returns the string’s acronym 
+  (first letter of each word capitalized). 
+
+  Do it with .split first if you need to, then try to do it without
+*/
+
+const str1 = "object oriented programming";
+const expected1 = "OOP";
+
+// The 4 pillars of OOP
+const str2 = "abstraction polymorphism inheritance encapsulation";
+const expected2 = "APIE";
+
+const str3 = "software development life cycle";
+const expected3 = "SDLC";
+
+// Bonus: ignore extra spaces
+const str4 = "  global   information tracker    ";
+const expected4 = "GIT";
+
+
+function acronymize(str) {
+
+    const splitString = str.toUpperCase().split(' ');
+    var answer = ""
+
+    for (i = 0; i < splitString.length; i++)
+        (splitString[i] != '') ? answer += splitString[i][0] : false;
+
+    return answer;
+}
+
+console.log(acronymize(str1) + " should be " + expected1)
+console.log(acronymize(str2) + " should be " + expected2)
+console.log(acronymize(str3) + " should be " + expected3)
+console.log(acronymize(str4) + " should be " + expected4)
