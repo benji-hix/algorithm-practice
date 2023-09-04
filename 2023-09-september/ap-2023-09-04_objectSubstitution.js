5ad8e9ea81ac88d06e60760abc5171211e4555bb
Benji Hix <benji.hix@outlook.com>
2023-09-04 13:28:11 -0700

    second algo

--- 2023-09-september/ap-2023-09-04_objectSubstitution.js
+++ 2023-09-september/ap-2023-09-04_objectSubstitution.js
@@ -64,3 +64,53 @@ console.log(alphabetPosition("The sunset sets at twelve o'clock."))
 console.log("20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11")
 console.log(alphabetPosition("The narwhal bacons at midnight"))
 console.log("20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20")
+
+// algo 2
+// You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+// Examples:
+// strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+// Concatenate the consecutive strings of strarr by 2, we get:
+
+// treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+// folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+// trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+// blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+// abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+// Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+// The first that came is "folingtrashy" so 
+// longest_consec(strarr, 2) should return "folingtrashy".
+
+// In the same way:
+// longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+// n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
+
+function longestConcat(strarr, k) {
+    let longestConcatStr = '' 
+    if (strarr.length == 0 || k > strarr.length || k <= 0) return longestConcatStr;
+    for (let i = 0; i < k; i++) {
+        longestConcatStr += strarr[i]
+    }
+
+    for (i = 0; i < strarr.length; i++) {
+        if (i + k >= strarr.length - 1)  break;
+        testConcat = ''
+        for (j = 0; j < k; j++) {
+            testConcat += strarr[i + j]
+        }
+        if (testConcat.length > longestConcatStr.length) longestConcatStr = testConcat;
+    }
+
+    return longestConcatStr
+}
+
+
+strarr1 = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"]
+k1 = 2
+console.log(longestConcat(strarr1, k1))
+
+strarr2 = ["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"]
+k2 = 2
+console.log(longestConcat(strarr2, k2))
