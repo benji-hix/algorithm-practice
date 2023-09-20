39bef246584030403962eb4a66af5b799724e031
Benji Hix <benji.hix@outlook.com>
2023-09-19 18:13:44 -0700

    daily algo

--- 2023-09-september/ap-2023-09-18_recursion.js
+++ 2023-09-september/ap-2023-09-18_recursion.js
@@ -27,23 +27,21 @@ const expected3 = 0;
 const nums4 = [1, 2, 3, 4, 5];
 const expected4 = 15;
 
-
 //* variation using an index as an arguent
-function sumArr(nums, index=0) {
+function sumArr(nums, index = 0) {
+    if (index >= nums.length) return 0;
 
-    if (index >= nums.length) return 0
-
-    return nums[index] + sumArr(nums, index+1)
+    return nums[index] + sumArr(nums, index + 1);
 }
+
 //* variation using pop()
-function sumArr(nums, index=0) {
+function sumArr(nums, index = 0) {
+    if (nums.length === 0) return 0;
 
-    if (nums.length === 0) return 0
-    
-    return nums.pop() + sumArr(nums)
+    return nums.pop() + sumArr(nums);
 }
 
-console.log(sumArr(nums1))
-console.log(sumArr(nums2))
-console.log(sumArr(nums3))
-console.log(sumArr(nums4))
+console.log(sumArr(nums1));
+console.log(sumArr(nums2));
+console.log(sumArr(nums3));
+console.log(sumArr(nums4));
